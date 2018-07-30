7cf64433493d5a0e24811be0e3cdb0ef0bfec0eb
kid <tjzyex@163.com>
2018-07-30 16:53:58 +0800

    152. Maximum Product Subarray

--- test.py
+++ test.py
@@ -1,41 +1,23 @@
 class Solution:
-    def lengthOfLIS(self, nums):
-        res = []
-        if len(nums) <= 0:
+
+    def maxProduct(self, nums):
+        if nums == []:
             return 0
+        min_res = 1
+        max_res = 1
+        res = [1] * len(nums)
         for i, n in enumerate(nums):
-            tmp = [1]
-            for j in range(i):
-                cnt = 1
-                if n > nums[i - j - 1]:
-                    tmp.append(cnt + res[i - j - 1])
-            res.append(max(tmp))
+            if n == 0:
+                min_res = 1
+                max_res = 1
+                res[i] = 0
+            else:
+                tmp = [n * max_res, n * min_res, n]
+                max_res = max(tmp)
+                min_res = min(tmp)
+                res[i] = max_res
         return max(res)
 
 
-# class Solution:
-#     def lengthOfLIS(self, nums):
-#         if len(nums) == 0:
-#             return 0
-#         if len(nums) == 1:
-#             return 1
-#         track = [1] * len(nums)
-#
-#         for i in range(1, len(nums)):
-#             for j in range(i):
-#                 if nums[j] < nums[i]:
-#                     if track[j] + 1 > track[i]:
-#                         track[i] = track[j] + 1
-#
-#     return max(track)
-
-
-import time
-
 s = Solution()
-start = time.clock()
-
-s.lengthOfLIS([1, 3, 6, 7, 9, 4, 10, 5, 6])
-
-end = time.clock()
-print(end - start)
+print(s.maxProduct([-3, -4, -5]))
