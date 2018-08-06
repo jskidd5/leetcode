531a6879780b7b32239c0f945b3e5fe36eef36ff
kid <tjzyex@163.com>
2018-08-06 10:51:07 +0800

    91.Decode Ways

--- test.py
+++ test.py
@@ -1,24 +1,34 @@
+from itertools import combinations
+
+
 class Solution:
-    def uniquePaths(self, m, n):
+    def numDecodings(self, s):
         """
-        :type m: int
-        :type n: int
+        :type s: str
         :rtype: int
         """
-        if m == 0 or n == 0:
+        len_s = len(s)
+        dp = [0] * len_s
+        if len_s <= 0:
             return 0
-        dp = []
-        for i in range(m):
-            dp.append(n * [0])
-        dp[0][0] = 1
-        for i in range(m):
-            for j in range(n):
-                if i - 1 >= 0:
-                    dp[i][j] += dp[i - 1][j]
-                if j - 1 >= 0:
-                    dp[i][j] += dp[i][j - 1]
-        return dp[m - 1][n - 1]
+        if s[0] == '0':
+            return 0
+        if len_s <= 1:
+            return 1
+        dp[0] = 1
+        if s[1] != '0':
+            dp[1] += 1
+        if int(s[0:2]) <= 26:
+            dp[1] += 1
+        for i in range(2, len_s):
+            if s[i] != '0':
+                dp[i] += dp[i - 1]
+            if 1 <= int(s[i - 1:i + 1]) <= 26 and s[i - 1] != '0':
+                dp[i] += dp[i - 2]
+            if dp[i] == 0:
+                return 0
+        return dp[-1]
 
 
 s = Solution()
-print(s.uniquePaths(1,0))
+print(s.numDecodings('101'))
