db6177851756965e0f583cb3dc54606364a113f4
kid <tjzyex@163.com>
2018-08-07 14:53:44 +0800

    416. Partition Equal Subset Sum

--- test.py
+++ test.py
@@ -1,21 +1,18 @@
 class Solution:
-    def combinationSum4(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        len_n = len(nums)
-        if len_n <= 0:
-            return 0
-        dp = [0] * (target + 1)
-        dp[0] = 1
-        for i in range(target + 1):
-            for n in nums:
-                if n <= i:
-                    dp[i] += dp[i - n]
-        return dp[-1]
-
+    def canPartition(self, nums):
+        s=sum(nums)
+        if s%2!=0:
+            return False
+        else:
+            s=s//2
+        dp=[False for x in range(s+1)]
+        dp[0]=True
+        for n in nums:
+            for i in range(s, n-1, -1):
+                if dp[i - n]:
+                    dp[i] = True
+            print(dp)
+        return dp
 
 s = Solution()
-print(s.combinationSum4([], 0))
+print(s.canPartition([1, 5, 11, 5]))
