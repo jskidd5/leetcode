29fdf5417a8e2b3d5ba4aae9727634a1b244b3e3
kid <tjzyex@163.com>
2018-08-09 11:19:38 +0800

    646. Maximum Length of Pair Chain

--- test.py
+++ test.py
@@ -1,18 +1,22 @@
 class Solution:
-    def canPartition(self, nums):
-        s=sum(nums)
-        if s%2!=0:
-            return False
-        else:
-            s=s//2
-        dp=[False for x in range(s+1)]
-        dp[0]=True
-        for n in nums:
-            for i in range(s, n-1, -1):
-                if dp[i - n]:
-                    dp[i] = True
-            print(dp)
+    def findLongestChain(self, pairs):
+        """
+        :type pairs: List[List[int]]
+        :rtype: int
+        """
+        len_p = len(pairs)
+        if len_p <= 0:
+            return 0
+        pairs.sort()
+        dp = [0] * len_p
+        dp[0] = 1
+        for i in range(1, len_p):
+            for j in range(i):
+                if pairs[i][0] > pairs[j][1]:
+                    dp[i] = max(dp[j], dp[i])
+            dp[i] += 1
         return dp
 
+
 s = Solution()
-print(s.canPartition([1, 5, 11, 5]))
+print(s.findLongestChain([[-10, -8], [-6, -4], [-5, 0], [-4, 7], [1, 7], [6, 10], [8, 9], [9, 10]]))
