10957f3dfc6bd890ec1804851b8f36f6ff1453b3
kid <tjzyex@163.com>
2018-08-27 16:18:39 +0800

    516. Longest Palindromic Subsequence

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
 class Solution:
-    def findLongestChain(self, pairs):
+    def longestPalindromeSubseq(self, s):
         """
-        :type pairs: List[List[int]]
+        :type s: str
         :rtype: int
         """
-        len_p = len(pairs)
-        if len_p <= 0:
-            return 0
-        pairs.sort()
-        dp = [0] * len_p
-        dp[0] = 1
-        for i in range(1, len_p):
-            for j in range(i):
-                if pairs[i][0] > pairs[j][1]:
-                    dp[i] = max(dp[j], dp[i])
-            dp[i] += 1
+        len_s = len(s)
+        dp = []
+        for i in range(len_s):
+            dp.append([0] * len_s)
+        for i in range(len_s):
+            dp[i][i] = 1
+            for j in range(i - 1, -1, -1):
+                if s[j] == s[i]:
+                    dp[i][j] = dp[i - 1][j + 1] + 2
+                else:
+                    dp[i][j] = max(dp[i][j + 1], dp[i - 1][j])
+
         return dp
 
 
 s = Solution()
-print(s.findLongestChain([[-10, -8], [-6, -4], [-5, 0], [-4, 7], [1, 7], [6, 10], [8, 9], [9, 10]]))
+print(s.longestPalindromeSubseq('bawbaob'))
