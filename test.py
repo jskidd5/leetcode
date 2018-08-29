c0c7ea68f648b9b720cbdb01e38d3ee2bd620a0c
kid <tjzyex@163.com>
2018-08-29 17:35:05 +0800

    790. Domino and Tromino Tiling

--- test.py
+++ test.py
@@ -1,30 +1,28 @@
 class Solution:
-    def countSubstrings(self, s):
+    def numTilings(self, N):
         """
-        :type s: str
+        :type N: int
         :rtype: int
         """
-        len_s = len(s)
-        dp = [1] * len_s
-        res = 0
-        for i in range(len_s):
-            res += 1
-            cnt = 1
-            while i - cnt + 1 >= 0 and i + cnt < len_s:
-                if s[i - cnt + 1] == s[i + cnt]:
-                    res += 1
-                else:
-                    break
-                cnt += 1
-            cnt = 1
-            while i - cnt >= 0 and i + cnt < len_s:
-                if s[i - cnt] == s[i + cnt]:
-                    res += 1
-                else:
-                    break
-                cnt += 1
-        return res
+        mod = 1000000007
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+        if N == 2:
+            return 2
+        if N == 3:
+            return 5
+        dp = [0] * (N + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+        for i in range(4, N + 1):
+            dp[i] = 2 * dp[i - 1] + dp[i - 3]
+            dp[i] = dp[i] % mod
+        return dp
 
 
 s = Solution()
-print(s.countSubstrings('aaa'))
+print(s.numTilings(5))
