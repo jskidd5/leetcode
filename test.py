23fb76eb445c157595ea8c2d21e20490bccdcdf8
kid <tjzyex@163.com>
2018-09-12 15:18:33 +0800

    740. Delete and Earn

--- test.py
+++ test.py
@@ -1,30 +1,22 @@
 class Solution:
-    def minimumDeleteSum(self, s1, s2):
+    def deleteAndEarn(self, nums):
         """
-        :type s1: str
-        :type s2: str
+        :type nums: List[int]
         :rtype: int
         """
-        len_s1 = len(s1)
-        len_s2 = len(s2)
-        dp = []
-        for i in range(len_s1 + 1):
-            dp.append([0] * (len_s2 + 1))
-        for i in range(1, len_s1 + 1):
-            dp[i][0] = dp[i - 1][0] + ord(s1[i - 1])
-        for i in range(1, len_s2 + 1):
-            dp[0][i] = dp[0][i - 1] + ord(s2[i - 1])
-        for i in range(1, len_s1 + 1):
-            for j in range(1, len_s2 + 1):
-                if s1[i - 1] == s2[j - 1]:
-                    tmp = 0
-                else:
-                    tmp = ord(s1[i - 1]) + ord(s2[j - 1])
-                dp[i][j] = min(dp[i - 1][j - 1] + tmp, dp[i - 1][j] + ord(s1[i - 1]), dp[i][j - 1] + ord(s2[j - 1]))
-        return dp[-1][-1]
+        len_n = len(nums)
+        map_n = {}
+        for i in range(len_n):
+            if nums[i] in map_n:
+                map_n[nums[i]] += 1
+            else:
+                map_n[nums[i]] = 1
+        dp = [0] * 10001
+        dp[1] = map_n.get(1, 0)
+        for i in range(2, 10001):
+            dp[i] = max(dp[i - 1], dp[i - 2] + i * map_n.get(i, 0))
+        return dp[-1]
 
 
 s = Solution()
-print(s.minimumDeleteSum(
-    "cicsddawqxrotwfehpfzxjukgumtcplecvgumpfitwowmtuhlgcbssisieoqgsfvavvaordkzwxpnppfjurckhyzeugkjowqzvojzigkrehiapcsiwwhvchrzhcsdgqyaovqzgbicnsstcofnmiipqxmjldknkcojlhzdprkqrqzbybexhmcfxhezzbwbfcbvnxrhatlklhadltciubrubudxthujdhtipicnvgangdpqicshcmmoacaonfsssjujzkznmxamgvlmaleovrcqijsdhnr",
-    "zkhkcdscdrgpqjqtksxiztcmymfgttiqntizhfafbvmjbjurxtbtnwilunqfmkkuvtgjzeoiunmqqbqoppejtzafjpbickphuvvmuhzvuxmsoeezvtlgfxjjjuwieobwldmyrfamokmewopvnmoiymfevorwanmicfumifjmrrhreeqkvmqgmwdfcmhxpujcbyfwdwqkbzovzuxzntwksedzzkdnqjpxhsqmffiwtxjedlbcpgfnetafbdxibsbntvqahhtbmbtdcgiedwdjnfbeqnydooasjdvxcyqqojiretcgeeycjrqqlxtuujmvzqrsrazvifeszibswkreosvzaexridrrcwptjavetgiiockenusrwsufnvskeumjiyvmyyadsqdwectoamfvpvcsalqtnpettilatie"))
+print(s.deleteAndEarn([1]))
