284f1f7e220b308fde43e46f6ff53f62645f3c37
kid <tjzyex@163.com>
2018-09-13 17:56:30 +0800

    764. Largest Plus Sign

--- test.py
+++ test.py
@@ -1,22 +1,53 @@
 class Solution:
-    def deleteAndEarn(self, nums):
+    def orderOfLargestPlusSign(self, N, mines):
         """
-        :type nums: List[int]
+        :type N: int
+        :type mines: List[List[int]]
         :rtype: int
         """
-        len_n = len(nums)
-        map_n = {}
-        for i in range(len_n):
-            if nums[i] in map_n:
-                map_n[nums[i]] += 1
-            else:
-                map_n[nums[i]] = 1
-        dp = [0] * 10001
-        dp[1] = map_n.get(1, 0)
-        for i in range(2, 10001):
-            dp[i] = max(dp[i - 1], dp[i - 2] + i * map_n.get(i, 0))
-        return dp[-1]
+        dp = []
+        up = []
+        down = []
+        left = []
+        right = []
+        for i in range(N):
+            dp.append([1] * N)
+            up.append([0] * N)
+            down.append([0] * N)
+            left.append([0] * N)
+            right.append([0] * N)
+        for i in mines:
+            dp[i[0]][i[1]] = 0
+        for i in range(0, N):
+            for j in range(0, N):
+                if dp[i][j] == 1:
+                    if i >= 1 and dp[i - 1][j] == 1:
+                        up[i][j] = up[i - 1][j] + 1
+                    else:
+                        up[i][j] = 1
+                if dp[N - i - 1][j] == 1:
+                    if i > 0 and dp[N - i][j] == 1:
+                        down[N - i - 1][j] = down[N - i][j] + 1
+                    else:
+                        down[N - i - 1][j] = 1
+                if dp[i][j] == 1:
+                    if j >= 1 and dp[i][j - 1] == 1:
+                        left[i][j] = left[i][j - 1] + 1
+                    else:
+                        left[i][j] = 1
+                if dp[i][N - j - 1] == 1:
+                    if j > 0 and dp[i][N - j] == 1:
+                        right[i][N - j - 1] = right[i][N - j] + 1
+                    else:
+                        right[i][N - j - 1] = 1
+        res = []
+        for i in range(0, N):
+            for j in range(0, N):
+                if dp[i][j] == 1:
+                    dp[i][j] = min(up[i][j], down[i][j], left[i][j], right[i][j])
+            res.append(max(dp[i]))
+        return max(res)
 
 
 s = Solution()
-print(s.deleteAndEarn([1]))
+print(s.orderOfLargestPlusSign(1, [[0, 0]]))
