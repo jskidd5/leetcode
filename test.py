29fbf5cf721065b7c8662d7f1d6e67ec0caccd6a
kid <tjzyex@163.com>
2018-09-17 17:18:12 +0800

    801. Minimum Swaps To Make Sequences Increasing

--- test.py
+++ test.py
@@ -1,53 +1,27 @@
 class Solution:
-    def orderOfLargestPlusSign(self, N, mines):
+    def minSwap(self, A, B):
         """
-        :type N: int
-        :type mines: List[List[int]]
+        :type A: List[int]
+        :type B: List[int]
         :rtype: int
         """
-        dp = []
-        up = []
-        down = []
-        left = []
-        right = []
-        for i in range(N):
-            dp.append([1] * N)
-            up.append([0] * N)
-            down.append([0] * N)
-            left.append([0] * N)
-            right.append([0] * N)
-        for i in mines:
-            dp[i[0]][i[1]] = 0
-        for i in range(0, N):
-            for j in range(0, N):
-                if dp[i][j] == 1:
-                    if i >= 1 and dp[i - 1][j] == 1:
-                        up[i][j] = up[i - 1][j] + 1
-                    else:
-                        up[i][j] = 1
-                if dp[N - i - 1][j] == 1:
-                    if i > 0 and dp[N - i][j] == 1:
-                        down[N - i - 1][j] = down[N - i][j] + 1
-                    else:
-                        down[N - i - 1][j] = 1
-                if dp[i][j] == 1:
-                    if j >= 1 and dp[i][j - 1] == 1:
-                        left[i][j] = left[i][j - 1] + 1
-                    else:
-                        left[i][j] = 1
-                if dp[i][N - j - 1] == 1:
-                    if j > 0 and dp[i][N - j] == 1:
-                        right[i][N - j - 1] = right[i][N - j] + 1
-                    else:
-                        right[i][N - j - 1] = 1
-        res = []
-        for i in range(0, N):
-            for j in range(0, N):
-                if dp[i][j] == 1:
-                    dp[i][j] = min(up[i][j], down[i][j], left[i][j], right[i][j])
-            res.append(max(dp[i]))
-        return max(res)
+        len_n = len(A)
+        if len_n <= 0:
+            return 0
+        dp1 = [0] * len_n
+        dp2 = [0] * len_n
+        for i in range(len_n):
+            dp1[i] = i
+            dp2[i] = i + 1
+        for i in range(1, len_n):
+            if A[i] > A[i - 1] and B[i] > B[i - 1]:
+                dp1[i] = dp1[i - 1]
+                dp2[i] = dp2[i - 1] + 1
+            if B[i] > A[i - 1] and A[i] > B[i - 1]:
+                dp1[i] = min(dp1[i], dp2[i - 1])
+                dp2[i] = dp1[i - 1] + 1
+        return min(dp1[-1], dp2[-1])
 
 
 s = Solution()
-print(s.orderOfLargestPlusSign(1, [[0, 0]]))
+print(s.minSwap([0, 4, 4, 5, 9], [0, 1, 6, 8, 10]))
