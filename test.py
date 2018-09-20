7a930041269014268c2c959a297f969c4cb17fe3
kid <tjzyex@163.com>
2018-09-20 15:43:47 +0800

    813. Largest Sum of Averages

--- test.py
+++ test.py
@@ -1,27 +1,27 @@
 class Solution:
-    def minSwap(self, A, B):
+    def largestSumOfAverages(self, A, K):
         """
         :type A: List[int]
-        :type B: List[int]
-        :rtype: int
+        :type K: int
+        :rtype: float
         """
-        len_n = len(A)
-        if len_n <= 0:
-            return 0
-        dp1 = [0] * len_n
-        dp2 = [0] * len_n
-        for i in range(len_n):
-            dp1[i] = i
-            dp2[i] = i + 1
-        for i in range(1, len_n):
-            if A[i] > A[i - 1] and B[i] > B[i - 1]:
-                dp1[i] = dp1[i - 1]
-                dp2[i] = dp2[i - 1] + 1
-            if B[i] > A[i - 1] and A[i] > B[i - 1]:
-                dp1[i] = min(dp1[i], dp2[i - 1])
-                dp2[i] = dp1[i - 1] + 1
-        return min(dp1[-1], dp2[-1])
+        len_a = len(A)
+        sum_a = [0] * (len_a + 1)
+        for i in range(1, len_a + 1):
+            sum_a[i] = sum_a[i - 1] + A[i - 1]
+        dp = []
+        for i in range(len_a + 1):
+            dp.append([0.0] * (K + 1))
+        for i in range(1, len_a + 1):
+            k = 1
+            dp[i][k] = sum_a[i] / i
+            k += 1
+            while k <= i and k <= K:
+                for j in range(1, i):
+                    dp[i][k] = max(dp[i][k], dp[j][k - 1] + ((sum_a[i] - sum_a[j]) / (i - j)))
+                k += 1
+        return dp[-1][-1]
 
 
 s = Solution()
-print(s.minSwap([0, 4, 4, 5, 9], [0, 1, 6, 8, 10]))
+print(s.largestSumOfAverages([4, 1, 7, 5, 6, 2, 3], 4))
