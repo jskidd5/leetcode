b92cfc722e099db2af7a94ed9013e895aa29d07b
kid <tjzyex@163.com>
2018-09-21 15:00:55 +0800

    179. Largest Number

--- test.py
+++ test.py
@@ -1,27 +1,12 @@
 class Solution:
-    def largestSumOfAverages(self, A, K):
-        """
-        :type A: List[int]
-        :type K: int
-        :rtype: float
-        """
-        len_a = len(A)
-        sum_a = [0] * (len_a + 1)
-        for i in range(1, len_a + 1):
-            sum_a[i] = sum_a[i - 1] + A[i - 1]
-        dp = []
-        for i in range(len_a + 1):
-            dp.append([0.0] * (K + 1))
-        for i in range(1, len_a + 1):
-            k = 1
-            dp[i][k] = sum_a[i] / i
-            k += 1
-            while k <= i and k <= K:
-                for j in range(1, i):
-                    dp[i][k] = max(dp[i][k], dp[j][k - 1] + ((sum_a[i] - sum_a[j]) / (i - j)))
-                k += 1
-        return dp[-1][-1]
+    # @param {integer[]} nums
+    # @return {string}
+    def largestNumber(self, nums):
+        from functools import cmp_to_key
+        key = cmp_to_key(lambda x,y: int(y+x)-int(x+y))
+        res = ''.join(sorted(map(str, nums), key=key)).lstrip('0')
+        return res or '0'
 
 
 s = Solution()
-print(s.largestSumOfAverages([4, 1, 7, 5, 6, 2, 3], 4))
+print(s.largestNumber([1, 3, 2, 4]))
