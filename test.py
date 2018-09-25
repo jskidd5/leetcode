ffbc14b148b2798964f7169043cb5603053f5b75
kid <tjzyex@163.com>
2018-09-25 14:51:04 +0800

    56. Merge Intervals

--- test.py
+++ test.py
@@ -1,12 +1,41 @@
+import functools
+
+
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
 class Solution:
-    # @param {integer[]} nums
-    # @return {string}
-    def largestNumber(self, nums):
-        from functools import cmp_to_key
-        key = cmp_to_key(lambda x,y: int(y+x)-int(x+y))
-        res = ''.join(sorted(map(str, nums), key=key)).lstrip('0')
-        return res or '0'
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        # tmp = []
+        # for i in intervals:
+        #     tmp.append(Interval(i[0], i[1]))
+        # intervals = tmp
+        len_i = len(intervals)
+        if len_i == 0:
+            return intervals
+        res = []
+        intervals = sorted(intervals, key=(lambda x: x.start))
+        tmp = intervals[0]
+        for i in range(1, len_i):
+            if intervals[i].start <= tmp.end:
+                intervals[i].start = tmp.start
+                if intervals[i].end <= tmp.end:
+                    intervals[i].end = tmp.end
+            else:
+                res.append(tmp)
+            tmp = intervals[i]
+        res.append(tmp)
+        # for i in res:
+        #     print('%d    %d' % (i.start, i.end))
+        return res
 
 
 s = Solution()
-print(s.largestNumber([1, 3, 2, 4]))
+print(s.merge([[2, 3], [2, 2], [3, 3], [1, 3], [5, 7], [2, 2], [4, 6]]))
