84f452cf99fdff8734a58db79e5b5ee710690702
kid <tjzyex@163.com>
2018-10-11 19:16:48 +0800

    496. Next Greater Element I

--- test.py
+++ test.py
@@ -1,60 +1,25 @@
-class MyStack:
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        flg = False
+        res = []
+        for i in nums1:
+            index = list.index(nums2, i) + 1
+            while index < len(nums2):
+                if i < nums2[index]:
+                    flg = True
+                    break
+                index += 1
+            if flg:
+                res.append(nums2[index])
+            else:
+                res.append(-1)
+            flg = False
+        return res
 
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.stack = []
-        self.index = 0
-
-    def push(self, x):
-        """
-        Push element x onto stack.
-        :type x: int
-        :rtype: void
-        """
-        if len(self.stack) > self.index:
-            self.stack[self.index] = x
-        else:
-            self.stack.append(x)
-        self.index += 1
-
-    def pop(self):
-        """
-        Removes the element on top of the stack and returns that element.
-        :rtype: int
-        """
-        self.index -= 1
-
-    def top(self):
-        """
-        Get the top element.
-        :rtype: int
-        """
-        if self.index > 0:
-            return self.stack[self.index - 1]
-        else:
-            return None
-
-    def empty(self):
-        """
-        Returns whether the stack is empty.
-        :rtype: bool
-        """
-        return self.index == 0
-
-
-# Your MyStack object will be instantiated and called as such:
-obj = MyStack()
-obj.push(10)
-obj.push(11)
-obj.push(12)
-obj.push(13)
-param_2 = obj.pop()
-param_2 = obj.pop()
-obj.push(13)
-param_3 = obj.top()
-param_4 = obj.empty()
-print(param_2)
-print(param_3)
-print(param_4)
+s = Solution()
+print(s.nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]))
