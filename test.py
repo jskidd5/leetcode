ae456eefd4d73c807363411418ca4321a0bd3874
kid <tjzyex@163.com>
2018-11-05 15:11:27 +0800

    456. 132 Pattern

--- test.py
+++ test.py
@@ -1,45 +1,26 @@
 class Solution:
-    def backspaceCompare(self, S, T):
+    def find132pattern(self, nums):
         """
-        :type S: str
-        :type T: str
+        :type nums: List[int]
         :rtype: bool
         """
-        len_s = len(S)
-        len_t = len(T)
-        s_i = len_s - 1
-        s_cnt = 0
-        t_i = len_t - 1
-        t_cnt = 0
-        while s_i >= 0 or t_i >= 0:
-            while s_i >= 0 and (S[s_i] == '#' or s_cnt > 0):
-                if S[s_i] == '#':
-                    s_cnt += 1
-                    s_i -= 1
-                elif s_cnt > 0:
-                    s_cnt -= 1
-                    s_i -= 1
-            tmp_s = ''
-            if s_i >= 0:
-                tmp_s = S[s_i]
-                s_i -= 1
-            while t_i >= 0 and (T[t_i] == '#' or t_cnt > 0):
-                if T[t_i] == '#':
-                    t_cnt += 1
-                    t_i -= 1
-                elif t_cnt > 0:
-                    t_cnt -= 1
-                    t_i -= 1
-            tmp_t = ''
-            if t_i >= 0:
-                tmp_t = T[t_i]
-                t_i -= 1
-            if tmp_t != tmp_s:
-                return False
-        if s_i != t_i:
+        n = len(nums)
+        if n <= 2:
             return False
-        return True
+        two = min(nums) - 1
+        stack = []
+        for i in range(n - 1, -1, -1):
+            if nums[i] < two:
+                return True
+            while len(stack) > 0 and nums[i] > stack[-1]:
+                two = stack[-1]
+                stack.pop()
+                print(stack)
+            if len(stack) == 0 or stack[-1] > nums[i]:
+                stack.append(nums[i])
+            print(stack, two)
+        return False
 
 
 s = Solution()
-print(s.backspaceCompare("s#v", "v"))
+print(s.find132pattern([4, 7, 2, 3, 6]))
