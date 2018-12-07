9d7cb180f8fb3bccf1829e6971b0d9e47c7b7979
linux <tjzyex@163.com>
2018-12-07 15:48:29 +0800

    907. 子数组的最小值之和

--- test.py
+++ test.py
@@ -1,25 +1,49 @@
 class Solution:
-    def nextGreaterElements(self, nums):
+    def sumSubarrayMins(self, A):
         """
-        :type nums: List[int]
-        :rtype: List[int]
+        :type A: List[int]
+        :rtype: int   1000000007
         """
-        res = [-1] * len(nums)
-        stack = []
-        # for i in range(2 * len(nums) - 1, -1, -1):
-        #     while len(stack) > 0 and nums[stack[-1]] <= nums[i % len(nums)]:
-        #         stack.pop()
-        #     if len(stack) > 0:
-        #         res[i % len(nums)] = nums[stack[-1]]
-        #     stack.append(i % len(nums))
-        for i in range(2 * len(nums) - 1, -1, -1):
-            while len(stack) > 0 and stack[-1] <= nums[i % len(nums)]:
-                stack.pop()
-            if len(stack) > 0:
-                res[i % len(nums)] = stack[-1]
-            stack.append(nums[i % len(nums)])
-        return res
+        # len_a = len(A)
+        # right_array = [1] * len_a
+        # left_array = [1] * len_a
+        # res = 0
+        # mod = 10 ** 9 + 7
+        # stack = []
+        # for i in range(len_a):
+        #     for j in range(i - 1, -1, -1):
+        #         if A[i] < A[j]:
+        #             left_array[i] += 1
+        #         else:
+        #             break
+        #     for j in range(i + 1, len_a):
+        #         if A[i] <= A[j]:
+        #             right_array[i] += 1
+        #         else:
+        #             break
+        # for i in range(len_a):
+        #     res += (right_array[i] * left_array[i]) * A[i]
+        # return res % mod
+        ans = 0
+        maxindex = len(A) - 1
+        left = []
+        right = []
+        for i in range(0, maxindex + 1):
+            numleft = 1
+            while i - numleft >= 0 and A[i - numleft] > A[i]:
+                numleft += left[i - numleft]
+            left.append(numleft)
+            print(left)
+            numright = 1
+            while maxindex - i + numright <= maxindex and A[maxindex - i + numright] >= A[maxindex - i]:
+                numright += right[i - numright]
+            right.append(numright)
+            print(right)
+            print()
+        for i in range(0, maxindex + 1):
+            ans += left[i] * right[maxindex - i] * A[i]
+        return ans % 1000000007
 
 
 s = Solution()
-print(s.nextGreaterElements([4, 7, 2, 3, 6]))
+print(s.sumSubarrayMins([3, 4, 5, 1]))
