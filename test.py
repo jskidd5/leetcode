6c6716337846674446621fdd1a2416faf7be68ce
linux <tjzyex@163.com>
2019-01-08 11:42:35 +0800

    901. Online Stock Span

--- test.py
+++ test.py
@@ -1,27 +1,31 @@
-class Solution:
-    def decodeAtIndex(self, S, K):
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price):
         """
-        :type S: str
-        :type K: int
-        :rtype: str
+        :type price: int
+        :rtype: int
         """
-
-        size = 0
-        for s in S:
-            if s.isdigit():
-                size = size * int(s)
-            else:
-                size += 1
-        for s in reversed(S):
-            K = K % size
-            if s.isalpha() and 0 == K:
-                return s
-            if s.isdigit():
-                size = size / int(s)
-            else:
-                size -= 1
-        return K
+        cnt = 1
+        while len(self.stack) > 0 and price >= self.stack[-1][0]:
+            cnt += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append([price, cnt])
+        print(self.stack)
+        return self.stack[-1][1]
 
 
-s = Solution()
-print(s.decodeAtIndex("a2345678999999999999999",1))
+# Your StockSpanner object will be instantiated and called as such:
+l = ["next", "next", "next", "next", "next", "next", "next"]
+v = [[], [100], [80], [60], [70], [60], [75], [85]]
+obj = StockSpanner()
+cnt = 1
+for i in l:
+    param_1 = getattr(obj, i)(v[cnt][0])
+    cnt += 1
+    print(param_1)
+
+# s = Solution()
+# print(s.decodeAtIndex("a2345678999999999999999",1))
