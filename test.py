de958be905385efec28d22327a89dc0d5e786143
linux <tjzyex@163.com>
2019-01-14 16:51:45 +0800

    385. 迷你语法分析器(mini parser)

--- test.py
+++ test.py
@@ -1,31 +1,98 @@
-class StockSpanner:
+"""
+This is the interface that allows for creating nested lists.
+You should not implement it, or speculate about its implementation
+"""
 
-    def __init__(self):
-        self.stack = []
 
-    def next(self, price):
+class NestedInteger:
+
+    def __init__(self, value=None):
         """
-        :type price: int
-        :rtype: int
+        If value is not specified, initializes an empty list.
+        Otherwise initializes a single integer equal to value.
         """
-        cnt = 1
-        while len(self.stack) > 0 and price >= self.stack[-1][0]:
-            cnt += self.stack[-1][1]
-            self.stack.pop()
-        self.stack.append([price, cnt])
-        print(self.stack)
-        return self.stack[-1][1]
+        self.vec = []
+
+    def isInteger(self):
+        """
+        @return True if this NestedInteger holds a single integer, rather than a nested list.
+        :rtype bool
+        """
+
+    def add(self, elem):
+        """
+        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+        :rtype void
+        """
+        self.vec.append(elem)
+
+    def setInteger(self, value):
+        """
+        Set this NestedInteger to hold a single integer equal to value.
+        :rtype void
+        """
+
+    def getInteger(self):
+        """
+        @return the single integer that this NestedInteger holds, if it holds a single integer
+        Return None if this NestedInteger holds a nested list
+        :rtype int
+        """
+
+    def getList(self):
+        """
+        @return the nested list that this NestedInteger holds, if it holds a nested list
+        Return None if this NestedInteger holds a single integer
+        :rtype List[NestedInteger]
+        """
+        return self.vec
 
 
-# Your StockSpanner object will be instantiated and called as such:
-l = ["next", "next", "next", "next", "next", "next", "next"]
-v = [[], [100], [80], [60], [70], [60], [75], [85]]
-obj = StockSpanner()
-cnt = 1
-for i in l:
-    param_1 = getattr(obj, i)(v[cnt][0])
-    cnt += 1
-    print(param_1)
+#
+# class Solution:
+#     def deserialize(self, s):
+#         """
+#         :type s: str
+#         :rtype: NestedInteger
+#         """
+#         res = NestedInteger()
+#         tmp_nest = []
+#         nums = ''
+#         for i in s:
+#             if i == '[':
+#                 tmp_nest.append(NestedInteger())
+#             elif i == ']':
+#                 res.add(tmp_nest.pop())
+#             elif i == ',':
+#                 if nums != '':
+#                     tmp_nest[-1].add(int(nums))
+#                     nums = ''
+#             else:
+#                 nums += i
+#         if nums != '':
+#             res.setInteger(int(nums))
+#         return res
+class Solution:
+    def deserialize(self, s):
+        stack, num, last = [], "", None
+        for c in s:
+            if c.isdigit() or c == "-":
+                num += c
+            elif c == "," and num:
+                stack[-1].add(NestedInteger(int(num)))
+                num = ""
+            elif c == "[":
+                elem = NestedInteger()
+                if stack:
+                    stack[-1].add(elem)
+                stack.append(elem)
+            elif c == "]":
+                if num:
+                    stack[-1].add(NestedInteger(int(num)))
+                    num = ""
+                last = stack.pop()
+        return last if last else NestedInteger(int(num))
 
-# s = Solution()
-# print(s.decodeAtIndex("a2345678999999999999999",1))
+
+s = Solution()
+print(s.deserialize("[123,456,[788,799,833],[[]],10,[]]").getList())
