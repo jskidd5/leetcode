26adb4eb2b9fc8dea4afb48ed1c437915dcc28d7
linux <tjzyex@163.com>
2019-01-14 17:24:33 +0800

    946. Validate Stack Sequences

--- test.py
+++ test.py
@@ -1,98 +1,21 @@
-"""
-This is the interface that allows for creating nested lists.
-You should not implement it, or speculate about its implementation
-"""
-
-
-class NestedInteger:
-
-    def __init__(self, value=None):
-        """
-        If value is not specified, initializes an empty list.
-        Otherwise initializes a single integer equal to value.
-        """
-        self.vec = []
-
-    def isInteger(self):
-        """
-        @return True if this NestedInteger holds a single integer, rather than a nested list.
-        :rtype bool
-        """
-
-    def add(self, elem):
-        """
-        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
-        :rtype void
-        """
-        self.vec.append(elem)
-
-    def setInteger(self, value):
-        """
-        Set this NestedInteger to hold a single integer equal to value.
-        :rtype void
-        """
-
-    def getInteger(self):
-        """
-        @return the single integer that this NestedInteger holds, if it holds a single integer
-        Return None if this NestedInteger holds a nested list
-        :rtype int
-        """
-
-    def getList(self):
-        """
-        @return the nested list that this NestedInteger holds, if it holds a nested list
-        Return None if this NestedInteger holds a single integer
-        :rtype List[NestedInteger]
-        """
-        return self.vec
-
-
-#
-# class Solution:
-#     def deserialize(self, s):
-#         """
-#         :type s: str
-#         :rtype: NestedInteger
-#         """
-#         res = NestedInteger()
-#         tmp_nest = []
-#         nums = ''
-#         for i in s:
-#             if i == '[':
-#                 tmp_nest.append(NestedInteger())
-#             elif i == ']':
-#                 res.add(tmp_nest.pop())
-#             elif i == ',':
-#                 if nums != '':
-#                     tmp_nest[-1].add(int(nums))
-#                     nums = ''
-#             else:
-#                 nums += i
-#         if nums != '':
-#             res.setInteger(int(nums))
-#         return res
 class Solution:
-    def deserialize(self, s):
-        stack, num, last = [], "", None
-        for c in s:
-            if c.isdigit() or c == "-":
-                num += c
-            elif c == "," and num:
-                stack[-1].add(NestedInteger(int(num)))
-                num = ""
-            elif c == "[":
-                elem = NestedInteger()
-                if stack:
-                    stack[-1].add(elem)
-                stack.append(elem)
-            elif c == "]":
-                if num:
-                    stack[-1].add(NestedInteger(int(num)))
-                    num = ""
-                last = stack.pop()
-        return last if last else NestedInteger(int(num))
+    def validateStackSequences(self, pushed, popped):
+        """
+        :type pushed: List[int]
+        :type popped: List[int]
+        :rtype: bool
+        """
+        stack = []
+        len_push = len(pushed)
+        push_cnt = 0
+        while push_cnt < len_push:
+            stack.append(pushed[push_cnt])
+            push_cnt += 1
+            while stack and stack[-1] == popped[0]:
+                popped.pop(0)
+                stack.pop()
+        return len(popped) == 0
 
 
 s = Solution()
-print(s.deserialize("[123,456,[788,799,833],[[]],10,[]]").getList())
+print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 5, 3, 1, 2]))
