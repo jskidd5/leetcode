3d4b1857fad2f84f8d03bc71fe7ddcde8f5a9440
linux <tjzyex@163.com>
2019-01-16 15:44:26 +0800

    856. Score of Parentheses

--- test.py
+++ test.py
@@ -1,30 +1,24 @@
 class Solution:
-    def exclusiveTime(self, n, logs):
+    def scoreOfParentheses(self, S):
         """
-        :type n: int
-        :type logs: List[str]
-        :rtype: List[int]
+        :type S: str
+        :rtype: int
         """
         stack = []
-        times = []
-        last = 0
-        res = n * [0]
-        for log in logs:
-            tmp = log.split(':')
-            if tmp[1] == 'start':
-                stack.append([int(tmp[0]), int(tmp[2])])
-                times.append(0)
-            elif tmp[1] == 'end':
-                dec_time = 0
-                all_tmp_time = int(tmp[2]) - stack.pop()[1] + 1
-                while times and times[-1] != 0:
-                    dec_time += times.pop()
-                res[int(tmp[0])] += all_tmp_time - dec_time
-                times.pop()
-                times.append(all_tmp_time)
-            # print(stack, '-----', times, '====', res)
-        return res
+        for s in S:
+            if s == '(':
+                stack.append(-1)
+            elif s == ')':
+                tmp = 0
+                while stack and stack[-1] != -1:
+                    tmp += stack.pop()
+                stack.pop()
+                if tmp:
+                    stack.append(2 * tmp)
+                else:
+                    stack.append(1)
+        return sum(stack)
 
 
 s = Solution()
-print(s.exclusiveTime(2,["0:start:0","0:start:2","0:end:5","1:start:6","1:end:6","0:end:7"]))
+print(s.scoreOfParentheses('()()'))
