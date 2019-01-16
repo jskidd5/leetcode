a55e4845289d85529e3b5af8ec218823958ab825
linux <tjzyex@163.com>
2019-01-16 10:18:46 +0800

    853. Car Fleet

--- test.py
+++ test.py
@@ -1,21 +1,28 @@
 class Solution:
-    def validateStackSequences(self, pushed, popped):
+
+    def carFleet(self, target, position, speed):
         """
-        :type pushed: List[int]
-        :type popped: List[int]
-        :rtype: bool
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
         """
-        stack = []
-        len_push = len(pushed)
-        push_cnt = 0
-        while push_cnt < len_push:
-            stack.append(pushed[push_cnt])
-            push_cnt += 1
-            while stack and stack[-1] == popped[0]:
-                popped.pop(0)
-                stack.pop()
-        return len(popped) == 0
+        speed_stack = []
+        map_car = []
+        car_cnt = len(position)
+        if car_cnt == 0:
+            return 0
+        for i in range(car_cnt):
+            map_car.append([position[i], speed[i]])
+        map_car.sort(reverse=True)
+        speed_stack.append([map_car[0][1], (target - map_car[0][0]) / map_car[0][1]])
+        for i in range(1, car_cnt):
+            if map_car[i][1] > speed_stack[-1][0] and speed_stack[-1][1] * map_car[i][1] + map_car[i][0] >= target:
+                pass
+            else:
+                speed_stack.append([map_car[i][1], (target - map_car[i][0]) / map_car[i][1]])
+        return len(speed_stack)
 
 
 s = Solution()
-print(s.validateStackSequences([1, 2, 3, 4, 5], [4, 5, 3, 1, 2]))
+print(s.carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]))
