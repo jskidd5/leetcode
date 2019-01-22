b7ac1efd56ea03e091a206b96ca0c2c7c6756fc7
linux <tjzyex@163.com>
2019-01-22 16:27:58 +0800

    226. Invert Binary Tree

--- test.py
+++ test.py
@@ -7,38 +7,42 @@ class TreeNode:
 
 
 class Solution:
-    def isSameTree(self, p, q):
+    def invertTree(self, root):
         """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
+        :type root: TreeNode
+        :rtype: TreeNode
         """
-        res = True
-        stack = [[p, q]]
-        print(stack)
+        stack = []
+        if root:
+            stack.append(root)
         while stack:
-            root = stack.pop()
-            if root[0] and root[1]:
-                print(root[0].val)
-                if root[0].val != root[1].val:
-                    return False
-                else:
-                    stack.append([root[0].right, root[1].right])
-                    stack.append([root[0].left, root[1].left])
-            elif root[0] or root[1]:
-                return False
-        return res
+            r = stack.pop()
+            r.right, r.left = r.left, r.right
+            if r.right:
+                stack.append(r.right)
+            if r.left:
+                stack.append(r.left)
+        return root
 
 
 p = TreeNode(1)
 p.left = TreeNode(2)
-p.left.right = TreeNode(3)
-p.left.right.left = TreeNode(4)
+p.left.left = TreeNode(3)
+p.left.right = TreeNode(4)
 p.right = TreeNode(5)
 p.right.right = TreeNode(6)
 p.right.right.left = TreeNode(7)
-p.right.right.left.left = TreeNode(8)
-p.right.right.left.right = TreeNode(9)
+p.right.right.right = TreeNode(8)
+#p.right.right.left.right = TreeNode(9)
 
 s = Solution()
-print(s.isSameTree(p, p))
+print()
+res = s.invertTree(p)
+stack = [res]
+while stack:
+    r = stack.pop()
+    print(r.val)
+    if r.right:
+        stack.append(r.right)
+    if r.left:
+        stack.append(r.left)
