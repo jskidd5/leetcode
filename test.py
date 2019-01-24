616d33e118bf38a43b0cfb0207a2c76ebd3845bf
linux <tjzyex@163.com>
2019-01-24 14:39:09 +0800

    235. Lowest Common Ancestor of a Binary Search Tree

--- test.py
+++ test.py
@@ -7,42 +7,53 @@ class TreeNode:
 
 
 class Solution:
-    def invertTree(self, root):
+    def lowestCommonAncestor(self, root, p, q):
         """
         :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
         :rtype: TreeNode
         """
-        stack = []
-        if root:
-            stack.append(root)
-        while stack:
-            r = stack.pop()
-            r.right, r.left = r.left, r.right
-            if r.right:
-                stack.append(r.right)
-            if r.left:
-                stack.append(r.left)
-        return root
+        stack_p = [root]
+        stack_q = [root]
+        tmp = root
+        while tmp and p.val != tmp.val:
+            if tmp and tmp.val > p.val:
+                tmp = tmp.left
+                stack_p.append(tmp)
+            elif tmp and tmp.val < p.val:
+                tmp = tmp.right
+                stack_p.append(tmp)
+        tmp = root
+        while tmp and q.val != tmp.val:
+            if tmp and tmp.val > q.val:
+                tmp = tmp.left
+                stack_q.append(tmp)
+            elif tmp and tmp.val < q.val:
+                tmp = tmp.right
+                stack_q.append(tmp)
+        while len(stack_p) > len(stack_q):
+            stack_p.pop()
+        while len(stack_p) < len(stack_q):
+            stack_q.pop()
+        while stack_p[-1].val != stack_q[-1].val:
+            stack_p.pop()
+            stack_q.pop()
+        return stack_p[-1]
 
 
-p = TreeNode(1)
+p = TreeNode(6)
 p.left = TreeNode(2)
-p.left.left = TreeNode(3)
+p.left.left = TreeNode(0)
 p.left.right = TreeNode(4)
-p.right = TreeNode(5)
-p.right.right = TreeNode(6)
-p.right.right.left = TreeNode(7)
-p.right.right.right = TreeNode(8)
-#p.right.right.left.right = TreeNode(9)
+p.left.right.left = TreeNode(3)
+p.left.right.right = TreeNode(5)
+p.right = TreeNode(8)
+p.right.left = TreeNode(7)
+p.right.right = TreeNode(9)
+
+# p.right.right.left.right = TreeNode(9)
 
 s = Solution()
-print()
-res = s.invertTree(p)
-stack = [res]
-while stack:
-    r = stack.pop()
-    print(r.val)
-    if r.right:
-        stack.append(r.right)
-    if r.left:
-        stack.append(r.left)
+res = s.lowestCommonAncestor(p, TreeNode(2), TreeNode(4))
+print(res.val)
