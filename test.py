f20a91a212f2a09a3547341c58aff3a1ce3f29db
linux <tjzyex@163.com>
2019-02-14 14:25:26 +0800

    606. Construct String from Binary Tree

--- test.py
+++ test.py
@@ -7,63 +7,62 @@ class TreeNode:
 
 
 class Solution:
-    def isSubtree(self, s, t):
+    def tree2str(self, t):
         """
-        :type s: TreeNode
         :type t: TreeNode
-        :rtype: bool
+        :rtype: str
         """
-        if s is None or t is None:
-            return False
-        root = s
-        cmp = []
-        stack = [root]
-        while stack and stack[-1]:
+        if t is None:
+            return ''
+        res = str(t.val)
+        stack = [[t, 0]]
+        last_h = -1
+        pair = 0
+        while t and len(stack) > 0:
             tmp = stack.pop()
-            if tmp.val == t.val:
-                cmp.append(tmp)
-            if tmp.right:
-                stack.append(tmp.right)
-            if tmp.left:
-                stack.append(tmp.left)
-        if len(cmp) == 0:
-            return False
-
-        for c in cmp:
-            res = True
-            stack = [[c, t]]
-            while stack and stack[-1]:
-                tmp = stack.pop()
-                if tmp[0].val != tmp[1].val:
-                    res = False
-                    break
-                if tmp[0].right and tmp[1].right:
-                    stack.append([tmp[0].right, tmp[1].right])
-                elif (tmp[0].right is None and tmp[1].right) or (tmp[0].right and tmp[1].right is None):
-                    res = False
-                    break
-                if tmp[0].left and tmp[1].left:
-                    stack.append([tmp[0].left, tmp[1].left])
-                elif (tmp[0].left is None and tmp[1].left) or (tmp[0].left and tmp[1].left is None):
-                    res = False
-                    break
-            if res:
-                return True
+            if last_h > tmp[1]:
+                cnt = last_h - tmp[1]
+                while pair and cnt:
+                    res += ')'
+                    pair -= 1
+                    cnt -= 1
+            if tmp[1] > 0:
+                res += '(' + str(tmp[0].val)
+                pair += 1
+            if tmp[0].right is None and tmp[0].left is None:
+                res += ')'
+                pair -= 1
+            elif tmp[0].left is None:
+                res += '()'
+            last_h = tmp[1]
+            if tmp[0].right:
+                stack.append([tmp[0].right, tmp[1] + 1])
+            if tmp[0].left:
+                stack.append([tmp[0].left, tmp[1] + 1])
+        if pair > 0:
+            while pair:
+                res += ')'
+                pair -= 1
+        else:
+            res = res[0:len(res) + pair]
         return res
 
 
-p = TreeNode(2)
+p = TreeNode(6)
 p.left = TreeNode(2)
-p.left.left = TreeNode(2)
-# p.left.right = TreeNode(4)
-# p.left.right.left = TreeNode(3)
-# p.left.right.right = TreeNode(5)
-# p.right = TreeNode(8)
-# p.right.left = TreeNode(7)
-# p.right.right = TreeNode(9)
+p.left.left = TreeNode(1)
+p.left.right = TreeNode(4)
+p.left.right.left = TreeNode(3)
+p.left.right.right = TreeNode(5)
+p.right = TreeNode(8)
+p.right.left = TreeNode(7)
+p.right.right = TreeNode(9)
 
-t = TreeNode(1)
-
+t = TreeNode(5)
+t.left = TreeNode(2)
+t.left.right = TreeNode(3)
+t.left.left = TreeNode(4)
+t.left.left.left = TreeNode(1)
 s = Solution()
-res = s.isSubtree(p, t)
+res = s.tree2str(t)
 print(res)
